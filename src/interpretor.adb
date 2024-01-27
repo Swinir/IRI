5e9573f332aa053117b2ba3765999daaff33bcbb
Swinir <jeanbaptiste.bruneau@hotmail.fr>
2024-01-27 16:47:17 +0100

    fix(interpretor) : fixed typo in parameter

--- src/interpretor.adb
+++ src/interpretor.adb
@@ -3,51 +3,51 @@ with Common_Types;
 
 package body Interpretor is
 
-    procedure Init(Path : in String; Intepreteur : out T_Interpretor) is
+    procedure Init(Path : in String; Interpreteur : out T_Interpretor) is
         Memoire :  Memory.T_Memory;
         Registre : Register.Register_Type;
         IR : Memory.T_Instructions;
     begin
         Register.Init(Registre);
         Memory.Init(Memoire);
-        Intepreteur.Memoire := Memoire;
-        Intepreteur.Registre := Registre;
-        Intepreteur.PC := 1;
+        Interpreteur.Memoire := Memoire;
+        Interpreteur.Registre := Registre;
+        Interpreteur.PC := 1;
 
-        Read_File_Content(Path, Intepreteur);
-        Intepreteur.IR := Memory.Get_Data(Intepreteur.Memoire, Intepreteur.PC);
+        Read_File_Content(Path, Interpreteur);
+        Interpreteur.IR := Memory.Get_Data(Interpreteur.Memoire, Interpreteur.PC);
     end Init;
 
 
 
-    procedure Read_File_Content(Path : in String; Intepreteur : out T_Interpretor) is
+    procedure Read_File_Content(Path : in String; Interpreteur : out T_Interpretor) is
         Handle : Reader.File_Handle;
         File_Content_String : Common_Types.String_List;
     begin
         Reader.Open_File(Path => Path, Handle => Handle);
         File_Content_String := Reader.Get_Lines(Handle => Handle);
         Reader.Close_File(Handle);
-        Lexer.Analyser_Lignes(File_Content_String, Intepreteur.Memoire);
+        Lexer.Analyser_Lignes(File_Content_String, Interpreteur.Memoire);
     end Read_File_Content;
 
-    function Get_PC(Intepreteur : in T_Interpretor) return Integer is
+    function Get_PC(Interpreteur : in T_Interpretor) return Integer is
     begin
-        return Intepreteur.PC;
+        return Interpreteur.PC;
     end Get_PC;
 
-    function Get_IR(Intepreteur : in T_Interpretor) return Memory.T_Instructions is
+    function Get_IR(Interpreteur : in T_Interpretor) return Memory.T_Instructions is
     begin
-        return Intepreteur.IR;
+        return Interpreteur.IR;
     end Get_IR;
 
-    function Get_Registre(Intepreteur : in T_Interpretor) return Register.Register_Type is 
+    function Get_Registre(Interpreteur : in T_Interpretor) return Register.Register_Type is 
     begin
-        return Intepreteur.Registre;
+        return Interpreteur.Registre;
     end Get_Registre;
 
-    function Get_Memory(Intepreteur : in T_Interpretor) return Memory.T_Memory is 
+    function Get_Memory(Interpreteur : in T_Interpretor) return Memory.T_Memory is 
     begin
-        return Intepreteur.Memoire;
+        return Interpreteur.Memoire;
     end Get_Memory;
 
     function Get_IR_Value(IR : in Memory.T_Instructions) return Unbounded_String is
@@ -55,58 +55,55 @@ package body Interpretor is
         Return IR.Token1 & IR.Token2 & IR.Token3 & IR.Token4;
     end Get_IR_Value;
 
-    procedure Increment_PC(Intepreteur : in out T_Interpretor) is
+    procedure Increment_PC(Interpreteur : in out T_Interpretor) is
     begin
-        Intepreteur.PC := Intepreteur.PC + 1;
+        Interpreteur.PC := Interpreteur.PC + 1;
     end Increment_PC;
 
-    procedure Display_Infos(Intepreteur : in T_Interpretor) is
+    procedure Display_Infos(Interpreteur : in T_Interpretor) is
     begin
-        --  Put_Line("----------- Memory Content -----------");
-        --  for Index in 1..Memory.Length(Intepreteur.Memoire) loop
-        --      Put_Line("Memory value position :" & Integer'Image(Index));
-        --      Memory.Put(Memory.Get_Data(Intepreteur.Memoire, Index));
-        --  end loop;
-        --  Put_Line("----------------------------------------");
-        Put("----------- PC Value : ");Put(Integer'Image(Intepreteur.PC));Put(" -------------");
-        Put_Line("----------------------------------------");
-        Put_Line("----------------------------------------");
-        Put("----------- IR Value : ");Put(" -------------");
-        Memory.Put(Intepreteur.IR);
-        Put_Line("----------------------------------------");
         Put_Line("");
-        Put_Line("----------- Register Content -----------");
-        for Index in 1..Register.Length(Intepreteur.Registre) loop
-            Put_Line("Register value position :" & Integer'Image(Index));
-            Register.Put(Register.Variable_List.Get_Data(Intepreteur.Registre, Index));
+        Put_Line("--------------------------------- Register Content ---------------------------------");
+        Put("-- Registry index value :" & Integer'Image(1) & "  |  ");
+        Register.Put(Register.Variable_List.Get_Data(Interpreteur.Registre, 1));
+        Put_Line("");
+        for Index in 2..Register.Length(Interpreteur.Registre) loop
+            Put_Line("------------------------------------------------");
+            Put("-- Registry index value :" & Integer'Image(Index) & "  |  ");
+            Register.Put(Register.Variable_List.Get_Data(Interpreteur.Registre, Index));
+            Put_Line("");
         end loop;
-        Put_Line("----------------------------------------");
+        Put_Line("----------------------------------------------------------------------------------");
+        Put_Line("");
     end Display_Infos;
 
-    procedure Display_Single_Info(Intepreteur : in T_Interpretor) is
+    procedure Display_Single_Info(Interpreteur : in T_Interpretor) is
     begin
-        Put("----------- PC Value : ");Put(Integer'Image(Intepreteur.PC));Put(" -------------");
-        Put_Line("----------- IR -----------");
-        Memory.Put(Intepreteur.IR);
-        Put_Line("--------------------------");
+        Put_Line("");
+        Put("--------------------------------- PC value :");Put(Integer'Image(Interpreteur.PC));Put(" ---------------------------------");
+        Put_Line("");
+        Put("-- IR Tokens : ");
+        Memory.Put(Interpreteur.IR);
+        Put_Line("");
+        Put_Line("----------------------------------------------------------------------------------");
     end Display_Single_Info;
 
 
-    procedure Interpret_Single_Instruction(Intepreteur : in out T_Interpretor) is
+    procedure Interpret_Single_Instruction(Interpreteur : in out T_Interpretor) is
     begin
-        if Intepreteur.PC <= Memory.Length(Intepreteur.Memoire) then
-            Evaluator.Evaluate_And_Execute(Intepreteur.IR,Intepreteur.Registre, Intepreteur.PC);
-            Increment_PC(Intepreteur);
-            Intepreteur.IR := Memory.Get_Data(Intepreteur.Memoire, Intepreteur.PC);
+        if Interpreteur.PC <= Memory.Length(Interpreteur.Memoire) then
+            Evaluator.Evaluate_And_Execute(Interpreteur.IR,Interpreteur.Registre, Interpreteur.PC);
+            Increment_PC(Interpreteur);
+            Interpreteur.IR := Memory.Get_Data(Interpreteur.Memoire, Interpreteur.PC);
         end if;
     end Interpret_Single_Instruction;
 
 
-    procedure Interpret_All(Intepreteur : in out T_Interpretor) is
+    procedure Interpret_All(Interpreteur : in out T_Interpretor) is
     begin
-        while (Intepreteur.PC <= Memory.Length(Intepreteur.Memoire) and not Evaluator.Is_End_Of_Program(Intepreteur.IR)) loop
-            Interpret_Single_Instruction(Intepreteur);
-            Interpretor.Display_Infos(Intepreteur); -- Debug
+        while (Interpreteur.PC <= Memory.Length(Interpreteur.Memoire) and not Evaluator.Is_End_Of_Program(Interpreteur.IR)) loop
+            Interpret_Single_Instruction(Interpreteur);
         end loop;
+        Display_Infos(Interpreteur);
     end Interpret_All;
 end Interpretor;
